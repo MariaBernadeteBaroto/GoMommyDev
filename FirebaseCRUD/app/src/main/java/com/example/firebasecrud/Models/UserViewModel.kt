6abe13b1d98dbd61a73bc2acdfd130d6f9c5ecdb
Maria Bernadete P. Baroto <92656206+MariaBernadeteBaroto@users.noreply.github.com>
2023-07-04 21:24:03 +0800

    Initial Commit

---
+++ FirebaseCRUD/app/src/main/java/com/example/firebasecrud/Models/UserViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.firebasecrud.Models
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.firebasecrud.Repository.UserRepository
+
+class UserViewModel : ViewModel() {
+
+    private val repository : UserRepository
+    private val _allUsers = MutableLiveData<List<User>>()
+    val allUsers : LiveData<List<User>> = _allUsers
+
+
+    init {
+
+        repository = UserRepository().getInstance()
+        repository.loadUsers(_allUsers)
+
+    }
+
+
+}
---
+++ FirebaseCRUD/app/src/main/java/com/example/firebasecrud/Repository/UserRepository.kt
@@ -0,0 +1,55 @@
+package com.example.firebasecrud.Repository
+
+import androidx.lifecycle.MutableLiveData
+import com.example.firebasecrud.Models.User
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
+class UserRepository {
+
+    private val databaseReference : DatabaseReference = FirebaseDatabase.getInstance().getReference("users")
+
+    @Volatile private var INSTANCE : UserRepository ?= null
+
+    fun getInstance() : UserRepository{
+
+        return INSTANCE ?: synchronized(this){
+
+            val instance = UserRepository()
+            INSTANCE = instance
+            instance
+        }
+    }
+
+
+    fun loadUsers(userList: MutableLiveData<List<User>>){
+
+        databaseReference.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                try {
+
+                    val _userList : List<User> = snapshot.children.map { dataSnapshot ->
+
+                        dataSnapshot.getValue(User::class.java)!!
+                    }
+
+                    userList.postValue(_userList)
+
+                } catch (e : Exception){
+
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
+
+    }
+
+
+}
