2a91a6ff92fe35c847497b716bacf022473d27ba
Maria Bernadete P. Baroto <92656206+MariaBernadeteBaroto@users.noreply.github.com>
2023-07-05 12:17:25 +0800

    Firebase Project Initialized

---
+++ Firebase Project/app/src/main/java/com/example/firebaseproject/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.firebaseproject
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        replaceFragment(Home())
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
+
+    }
+
+}
